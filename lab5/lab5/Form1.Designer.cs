7d9b085f95af04eace1565a4155de31834555786
elkhimova-irina <55272890+elkhimova-irina@users.noreply.github.com>
2019-12-09 17:31:00 +0300

    lab5
    lab5

---
+++ lab5/lab5/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            this.buttonExact = new System.Windows.Forms.Button();
+            this.buttonApprox = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.textBoxFileReadTime = new System.Windows.Forms.TextBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.textBoxExactTime = new System.Windows.Forms.TextBox();
+            this.textBoxApproxTime = new System.Windows.Forms.TextBox();
+            this.textBoxMaxDist = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonSaveReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Location = new System.Drawing.Point(54, 45);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(190, 79);
+            this.buttonLoadFile.TabIndex = 0;
+            this.buttonLoadFile.Text = "buttonLoadFile";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.buttonLoadFile_Click);
+            // 
+            // buttonExact
+            // 
+            this.buttonExact.Location = new System.Drawing.Point(54, 151);
+            this.buttonExact.Name = "buttonExact";
+            this.buttonExact.Size = new System.Drawing.Size(190, 70);
+            this.buttonExact.TabIndex = 1;
+            this.buttonExact.Text = "buttonExact";
+            this.buttonExact.UseVisualStyleBackColor = true;
+            this.buttonExact.Click += new System.EventHandler(this.buttonExact_Click_1);
+            // 
+            // buttonApprox
+            // 
+            this.buttonApprox.Location = new System.Drawing.Point(54, 250);
+            this.buttonApprox.Name = "buttonApprox";
+            this.buttonApprox.Size = new System.Drawing.Size(190, 70);
+            this.buttonApprox.TabIndex = 2;
+            this.buttonApprox.Text = "buttonApprox";
+            this.buttonApprox.UseVisualStyleBackColor = true;
+            this.buttonApprox.Click += new System.EventHandler(this.buttonApprox_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(517, 30);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(490, 179);
+            this.listBoxResult.TabIndex = 3;
+            // 
+            // textBoxFileReadTime
+            // 
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(299, 45);
+            this.textBoxFileReadTime.Name = "textBoxFileReadTime";
+            this.textBoxFileReadTime.Size = new System.Drawing.Size(168, 31);
+            this.textBoxFileReadTime.TabIndex = 4;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(311, 151);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(156, 31);
+            this.textBoxFind.TabIndex = 5;
+            // 
+            // textBoxExactTime
+            // 
+            this.textBoxExactTime.Location = new System.Drawing.Point(311, 190);
+            this.textBoxExactTime.Name = "textBoxExactTime";
+            this.textBoxExactTime.Size = new System.Drawing.Size(156, 31);
+            this.textBoxExactTime.TabIndex = 6;
+            // 
+            // textBoxApproxTime
+            // 
+            this.textBoxApproxTime.Location = new System.Drawing.Point(311, 289);
+            this.textBoxApproxTime.Name = "textBoxApproxTime";
+            this.textBoxApproxTime.Size = new System.Drawing.Size(156, 31);
+            this.textBoxApproxTime.TabIndex = 7;
+            // 
+            // textBoxMaxDist
+            // 
+            this.textBoxMaxDist.Location = new System.Drawing.Point(311, 250);
+            this.textBoxMaxDist.Name = "textBoxMaxDist";
+            this.textBoxMaxDist.Size = new System.Drawing.Size(156, 31);
+            this.textBoxMaxDist.TabIndex = 8;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(639, 377);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(149, 48);
+            this.buttonExit.TabIndex = 9;
+            this.buttonExit.Text = "buttonExit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonSaveReport
+            // 
+            this.buttonSaveReport.Location = new System.Drawing.Point(383, 377);
+            this.buttonSaveReport.Name = "buttonSaveReport";
+            this.buttonSaveReport.Size = new System.Drawing.Size(210, 48);
+            this.buttonSaveReport.TabIndex = 10;
+            this.buttonSaveReport.Text = "buttonSaveReport";
+            this.buttonSaveReport.UseVisualStyleBackColor = true;
+            this.buttonSaveReport.Click += new System.EventHandler(this.buttonSaveReport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 450);
+            this.Controls.Add(this.buttonSaveReport);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.textBoxMaxDist);
+            this.Controls.Add(this.textBoxApproxTime);
+            this.Controls.Add(this.textBoxExactTime);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.textBoxFileReadTime);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.buttonApprox);
+            this.Controls.Add(this.buttonExact);
+            this.Controls.Add(this.buttonLoadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadFile;
+        private System.Windows.Forms.Button buttonExact;
+        private System.Windows.Forms.Button buttonApprox;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.TextBox textBoxFileReadTime;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.TextBox textBoxExactTime;
+        private System.Windows.Forms.TextBox textBoxApproxTime;
+        private System.Windows.Forms.TextBox textBoxMaxDist;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonSaveReport;
+    }
+}
+
---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "текстовые файлы|*.txt";
+
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                string text = File.ReadAllText(f.FileName);
+
+                //разделительные знаки
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+
+                string[] textArray = text.Split(separators);
+
+                foreach (string strTemp in textArray)
+                {
+                    //удаление пробелов
+                    string str = strTemp.Trim();
+                    //добавление строки в список, если строка не содерж в списке
+                    if (!list.Contains(str)) list.Add(str);
+                }
+
+                t.Stop();
+                this.textBoxFileReadTime.Text = t.Elapsed.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void buttonExact_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+
+                //очистка списка
+                this.listBoxResult.Items.Clear();
+                //вывод результатов
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл и введите слово для поиска");
+            }
+        }
+
+        private void buttonApprox_Click(object sender, EventArgs e)
+        {
+
+            //Слово для поиска
+            string word = this.textBoxFind.Text.Trim();
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                int maxDist;
+                if (!int.TryParse(this.textBoxMaxDist.Text.Trim(), out maxDist))
+                {
+                    MessageBox.Show("Необходимо указать максимальное расстояние");
+                    return;
+                }
+                if (maxDist < 1 || maxDist > 5)
+                {
+                    MessageBox.Show("Максимальное расстояние должно быть в диапазоне от 1 до 5");
+                    return;
+                }
+                //Слово для поиска в верхнем регистре
+                string wordUpper = word.ToUpper();
+                //Временные результаты поиска
+                List<Tuple<string, int>> tempList = new List<Tuple<string, int>>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    //Вычисление расстояния Дамерау-Левенштейна
+                    int dist = EditDistance.Distance(str.ToUpper(), wordUpper);
+                    //Если расстояние меньше порогового, то слово добавляется в результат
+                    if (dist <= maxDist)
+                    {
+                        tempList.Add(new Tuple<string, int>(str, dist));
+
+                    }
+                }
+                t.Stop();
+                this.textBoxApproxTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                //Очистка списка
+                this.listBoxResult.Items.Clear();
+                //Вывод результатов поиска
+                foreach (var x in tempList)
+                {
+                    string temp = x.Item1 + " (расстояние = " + x.Item2.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+        private void buttonExact_Click_1(object sender, EventArgs e)
+        {
+            
+            string word = this.textBoxFind.Text.Trim();
+
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                //Слово для поиска в верхнем регистре
+                string wordUpper = word.ToUpper();
+
+                //Временные результаты поиска
+                List<string> tempList = new List<string>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+
+                this.listBoxResult.BeginUpdate();
+
+                //Очистка списка
+                this.listBoxResult.Items.Clear();
+
+                //Вывод результатов поиска 
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void buttonSaveReport_Click(object sender, EventArgs e)
+        {
+            //Имя файла отчета
+            string TempReportFileName = "Report_" + DateTime.Now.ToString("dd_MM_yyyy_hhmmss");
+
+            //Диалог сохранения файла отчета
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.FileName = TempReportFileName;
+            fd.DefaultExt = ".html";
+            fd.Filter = "HTML Reports|*.html";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string ReportFileName = fd.FileName;
+
+                //Формирование отчета
+                StringBuilder b = new StringBuilder();
+                b.AppendLine("<html>");
+
+                b.AppendLine("<head>");
+                b.AppendLine("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
+                b.AppendLine("<title>" + "Отчет: " + ReportFileName + "</title>");
+                b.AppendLine("</head>");
+
+                b.AppendLine("<body>");
+
+                b.AppendLine("<h1>" + "Отчет: " + ReportFileName + "</h1>");
+                b.AppendLine("<table border='1'>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время чтения из файла</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadTime.Text + "</td>");
+                b.AppendLine("</tr>");
+
+               /* b.AppendLine("<tr>");
+                b.AppendLine("<td>Количество уникальных слов в файле</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadCount.Text + "</td>");
+                b.AppendLine("</tr>"); */
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Слово для поиска</td>");
+                b.AppendLine("<td>" + this.textBoxFind.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Максимальное расстояние для нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxMaxDist.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время четкого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxExactTime.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxApproxTime.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr valign='top'>");
+                b.AppendLine("<td>Результаты поиска</td>");
+                b.AppendLine("<td>");
+                b.AppendLine("<ul>");
+
+                foreach (var x in this.listBoxResult.Items)
+                {
+                    b.AppendLine("<li>" + x.ToString() + "</li>");
+                }
+
+                b.AppendLine("</ul>");
+                b.AppendLine("</td>");
+                b.AppendLine("</tr>");
+
+
+                b.AppendLine("</table>");
+
+                b.AppendLine("</body>");
+                b.AppendLine("</html>");
+
+                //Сохранение файла
+                File.AppendAllText(ReportFileName, b.ToString());
+
+                MessageBox.Show("Отчет сформирован. Файл: " + ReportFileName);
+            }
+        }
+
+        private void textBoxFind_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
