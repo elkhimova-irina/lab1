102d3c07070350f19a4b0205218f794c5b4c1351
elkhimova-irina <55272890+elkhimova-irina@users.noreply.github.com>
2019-12-11 15:06:43 +0300

    lab6

---
+++ lab6/lab6/NewAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    class NewAttribute : Attribute
+    {
+        public NewAttribute() { }
+        public NewAttribute(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+
+        public string Description { get; set; }
+    }
+}
